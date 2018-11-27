893a3fcc54673139a50a98b44e7cf1d3c9faec54
andriyhrishin <agrishin@gmail.com>
2018-11-27 20:36:44 +0000

    unique partition names in partitioner

--- src/main/java/hello/remotepartition/BasicPartitioner.java
+++ src/main/java/hello/remotepartition/BasicPartitioner.java
@@ -1,8 +1,10 @@
 package hello.remotepartition;
 
 
+import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.batch.core.partition.support.Partitioner;
 import org.springframework.batch.core.partition.support.SimplePartitioner;
 import org.springframework.batch.item.ExecutionContext;
 
@@ -10,16 +12,22 @@ import org.springframework.batch.item.ExecutionContext;
  * Simple partitioner for demonstration purpose.
  *
  */
-public class BasicPartitioner extends SimplePartitioner {
+public class BasicPartitioner implements Partitioner {
 
     private static final String PARTITION_KEY = "partition";
 
     @Override
     public Map<String, ExecutionContext> partition(int gridSize) {
-        Map<String, ExecutionContext> partitions = super.partition(gridSize);
-        int i = 0;
-        for (ExecutionContext context : partitions.values()) {
-            context.put(PARTITION_KEY, PARTITION_KEY + (i++));
+        Map<String, ExecutionContext> partitions = new HashMap<>();
+        long j = System.currentTimeMillis();
+        for (int i = 0; i < gridSize; i++) {
+            ExecutionContext context = new ExecutionContext();
+            partitions.put(PARTITION_KEY+j, context);
+            context.put(PARTITION_KEY, PARTITION_KEY + j);
+            j++;
+        }
+        for (String key : partitions.keySet()) {
+            System.out.println("Sending " + key);
         }
         return partitions;
     }
