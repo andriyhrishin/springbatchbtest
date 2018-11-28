46d02e591670a8938e0b5fa6cca51c7b9861a716
andriyhrishin <agrishin@gmail.com>
2018-11-28 15:01:16 +0000

    latest spring boot run.txt - how to run some whistles and bells for job config

--- src/main/java/hello/remotepartition/MasterConfiguration.java
+++ src/main/java/hello/remotepartition/MasterConfiguration.java
@@ -4,17 +4,26 @@ import hello.BrokerConfiguration;
 import hello.DataSourceConfiguration;
 import org.apache.activemq.ActiveMQConnectionFactory;
 
+import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.JobRegistry;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.job.DefaultJobParametersValidator;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.integration.config.annotation.EnableBatchIntegration;
 import org.springframework.batch.integration.partition.RemotePartitioningMasterStepBuilderFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
 import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.jms.dsl.Jms;
+import org.springframework.transaction.PlatformTransactionManager;
 
 /**
  * This configuration class is for the master side of the remote partitioning sample.
@@ -30,6 +39,15 @@ import org.springframework.integration.jms.dsl.Jms;
 public class MasterConfiguration {
 
     private static final int GRID_SIZE = 3;
+
+    @Autowired
+    protected JobRepository jobRepository;
+    @Autowired
+    protected JobLauncher jobLauncher;
+    @Autowired
+    protected JobRegistry jobRegistry;
+    @Autowired
+    protected PlatformTransactionManager txManager;
 
     private final JobBuilderFactory jobBuilderFactory;
 
@@ -92,6 +110,20 @@ public class MasterConfiguration {
     public Job remotePartitioningJob() {
         return this.jobBuilderFactory.get("remotePartitioningJob")
                 .start(masterStep())
+                .preventRestart()
+                .listener(new JobExecutionListener() {
+                    public void beforeJob(JobExecution jobExecution) { System.out.println("JobExecutionListener.beforeJob: " + jobExecution.toString()); }
+                    public void afterJob(JobExecution jobExecution) {
+                        System.out.println("JobExecutionListener.afterJob: " + jobExecution.toString());
+                        if( jobExecution.getStatus() == BatchStatus.COMPLETED ){
+                            //job success
+                        }
+                        else if(jobExecution.getStatus() == BatchStatus.FAILED){
+                            //job failure
+                        }
+                    }
+                })
+                .validator(new DefaultJobParametersValidator(new String[] {"systime"}, new String[0]))
                 .build();
     }
 
